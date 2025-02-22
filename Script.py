5cd68ccab5bca0e6acc3ab47bcce85ede308814d
Thestarboi <lalxlclclc>
2025-02-22 10:54:17 +0530

    update

--- Script.py
+++ Script.py
@@ -271,7 +271,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/Deendayal_dhakad">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href=https://t.me/Deendayal_dhakad>[Deendayal_Dhakad]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/YKD_KOREAN_DRAMA">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a href=https://t.me/YKD_KOREAN_DRAMA>[Deendayal_Dhakad]</a></b>"""
 
     IMDB_TEMPLATE_TXT = """
 <b>🏷 Title</b>: <a href={url}>{title}</a>
