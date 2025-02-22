27e3254d9e00ffbe578dd9afda3f3734a1eab2ec
Thestarboi <lalckkfkflf>
2025-02-22 10:44:36 +0530

    update genlink.py

--- plugins/genlink.py
+++ plugins/genlink.py
@@ -40,7 +40,7 @@ async def gen_link_s(bot, message):
 @Client.on_message(filters.command(['batch', 'pbatch']) & filters.create(allowed))
 async def gen_link_batch(bot, message):
     if " " not in message.text:
-        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/HP_MOVIES_WORLD/10 https://t.me/HP_MOVIES_WORLD/20</code>.")
+        return await message.reply("Use correct format.\nExample <code>/batch https://t.me/Star_Boy_96_vibes/25 https://t.me/Star_Boy_96_vibes/30</code>.")
     links = message.text.strip().split(" ")
     if len(links) != 3:
         return await message.reply("Use correct format.\nExample <code>/batch https://t.me/All_Movies_Studio/10 https://t.me/All_Movies_Studio/20</code>.")
